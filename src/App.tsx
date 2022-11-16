6bedf434e4e1643a11fed35477f13a0787ca7aea
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2022-11-16 21:13:42 +0000

    progress save before experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import BasicInput from './components/BasicInput';
 
 class FormGen {
   components: Array<JSX.Element> = [];
-  fields: any;
+  formElements: any = []; // TODO: Make a FormElement interface.
 
   generate(){
+    const [state, setState] = useState({});
+
+    function updateFormField(fieldname: string){
+      
+    }
+
+    console.log(state);
+
     return () => (
     <div>
       { 
-        this.fields.map((field: any) => {
+        this.formElements.map((field: any) => {
           const component = {...this.components.find(component => component.type.name == field.component)};
-
           component.props = { name: field.name };
+          component.key = field.name;
 
-          return component
+          return component;
         })
       }
     </div>
@@ -23,7 +33,7 @@ class FormGen {
 
 const formGen = new FormGen()
 formGen.components = [<BasicInput/>];
-formGen.fields = [
+formGen.formElements = [
   { name: 'test_field', component: 'BasicInput' },
   { name: 'test_field2', component: 'BasicInput' }
 ];
@@ -43,9 +53,3 @@ function App() {
 }
 
 export default App
-
-function BasicInput({ name }: any) {
-  return (
-    <input type="text" name={name} placeholder={name} />
-  )
-}
--- src/components/BasicInput.tsx
+++ src/components/BasicInput.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react"
+
 function BasicInput({ name }: any) {
+    const [value, setValue] = useState('');
+
+    console.log(name, value);
     return (
-      <input type="text" name={name} placeholder={name} />
+      <input type="text" name={name} placeholder={name} value={value} onChange={e => setValue(e.target.value)} />
     )
   }
   
