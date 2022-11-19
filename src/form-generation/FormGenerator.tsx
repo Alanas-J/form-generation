255e26d9dae1068df9f6ffa9dfd6cddd6c94ae89
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2022-11-19 16:02:59 +0000

    validation refactoring and validate() method prop

--- src/form-generation/FormGenerator.tsx
+++ src/form-generation/FormGenerator.tsx
@@ -18,33 +18,12 @@ class FormGenerator {
       const [formState, setFormState] = useState<any>({_section: this.startOn});
       _setFormState = setFormState;
       _formState = formState;
-      
-      const setField = (name: string, value: any, group: string|undefined = undefined) => {
-        if(group){
-          if (formState[group] === undefined) formState[group] = {};
-          if (formState[group][name] === undefined) formState[group][name] = {};
-
-          formState[group][name].value = value;
-        } else {
-          if (formState[name] === undefined) formState[name] = {};
-          formState[name].value = value;
-        }
-        console.log(`Field ${group || ''}.${name} set to ${value}`, formState);
-        setFormState({...formState});
-      };
 
       return (
         <>
           { 
             this.sections[formState._section].elements.map((element: any) => {
               const Component = element.component;
-
-              const setValue = (value: any) => {
-                setField(element.name, value, element.group);
-              }
-              const validate = () => {
-                console.log('will validate in the future');
-              }
 
               let value = '';
               let validation = {};
@@ -55,6 +34,12 @@ class FormGenerator {
                 value = formState[element.name]?.value ?? '';
                 validation = formState[element.name]?.validation ?? {};
               }
+
+              const setValue = (value: any) => setField(element.name, element.group, value, formState, setFormState);
+              const validate = () => {
+                validateField(element, formState);
+                setFormState({...formState});
+              }
               
               const props = {
                 ...element,
@@ -64,7 +49,6 @@ class FormGenerator {
                 setFormState,
                 validation,
                 validate
-                // TODO: add a validate() function
               }
               return (<Component key={element.name} {...props}/>);
             })
@@ -74,16 +58,17 @@ class FormGenerator {
     };
 
     const formAction = (action: string) => {
+      let formState = _formState;
+      
       switch(action) {
         case 'next':
           if (_setFormState && this.sections[_formState._section].next) {
-            if(validateAllFields(this.sections[_formState._section].elements, _formState, _setFormState)) {
+            if(validateFields(this.sections[_formState._section].elements, formState)) {
+              formState = { ..._formState, _section: this.sections[_formState._section].next};
 
-              const formState = { ..._formState, _section: this.sections[_formState._section].next};
-             _setFormState(formState)
-
-             if(this.onStep) this.onStep(formState);
+              if(this.onStep) this.onStep(formState);
             }
+            _setFormState({...formState})
           }
           break;
         case 'previous':
@@ -106,40 +91,59 @@ class FormGenerator {
 }
 export default FormGenerator;
 
-function validateAllFields(elements: any, formState: any, setFormState: any): boolean {
+function setField( name: string, group: string|undefined, value: any, formState: any, setFormState: any) {
+  if(group){
+    if (formState[group] === undefined) formState[group] = {};
+    if (formState[group][name] === undefined) formState[group][name] = {};
+
+    formState[group][name].value = value;
+  } else {
+    if (formState[name] === undefined) formState[name] = {};
+    formState[name].value = value;
+  }
+  console.log(`Field ${group || ''}.${name} set to ${value}`, formState);
+  setFormState({...formState});
+};
+
+function validateField( element: any, formState: any) {
+  let error: boolean = false;
+
+  if(element.validations) {
+    for(const validate of element.validations) {
+      let result: any;
+      let value: any;
+
+      if(element.group){
+        if (formState[element.group] === undefined) formState[element.group] = {};
+        if (formState[element.group][element.name] === undefined) formState[element.group][element.name] = {};
+
+        value = formState[element.group][element.name]?.value;
+        result = validate(value);
+
+        formState[element.group][element.name].validation = result;
+      } else {
+        if (formState[element.name] === undefined) formState[element.name] = {};
+
+        value = formState[element.name]?.value;
+        result = validate(value);
+
+        formState[element.name].validation = result;
+      }
+
+      if(result.error === true){
+        error = error || result.error;
+        break;
+      }
+    }
+  }
+  return !error;
+}
+
+function validateFields(elements: any, formState: any): boolean {
   let error: boolean = false;
 
   elements.forEach((element: any) => {
-    if(element.validations){
-      for(const validate of element.validations){
-        let result: any;
-        let value: any;
-
-        if(element.group){
-          if (formState[element.group] === undefined) formState[element.group] = {};
-          if (formState[element.group][element.name] === undefined) formState[element.group][element.name] = {};
-
-          value = formState[element.group][element.name]?.value;
-          result = validate(value);
-
-          formState[element.group][element.name].validation = result;
-        } else {
-          if (formState[element.name] === undefined) formState[element.name] = {};
-
-          value = formState[element.name]?.value;
-          result = validate(value);
-
-          formState[element.name].validation = result;
-        }
-
-        if(result.error === true){
-          error = error || result.error
-          break;
-        }
-      }
-    }
-  })
-
-  if(error) setFormState({...formState});
+    error =  error || !validateField(element, formState)
+  });
   return !error;
 }
